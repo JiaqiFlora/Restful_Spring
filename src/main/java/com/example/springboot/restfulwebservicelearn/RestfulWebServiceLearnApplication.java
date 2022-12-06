b2d3ccafb1482caedae185866f326e046c6ff779
JiaqiFlora <jiaqigan.kiki@gmail.com>
2022-12-05 23:51:39 -0800

    init

---
+++ src/main/java/com/example/springboot/restfulwebservicelearn/RestfulWebServiceLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.restfulwebservicelearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServiceLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServiceLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot/restfulwebservicelearn/RestfulWebServiceLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.restfulwebservicelearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
